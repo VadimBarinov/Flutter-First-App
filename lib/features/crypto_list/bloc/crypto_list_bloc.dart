5102e2aa4fb56b24aaf918235065cd5fec241480
VadimBarinov <barinovvadim87@gmail.com>
2024-02-07 12:33:07 +0400

    10 lesson

--- lib/features/crypto_list/bloc/crypto_list_bloc.dart
+++ lib/features/crypto_list/bloc/crypto_list_bloc.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'crypto_list_event.dart';
@@ -9,10 +12,18 @@ class CryptoListBloc extends Bloc<CryptoListEvent, CryptoListState> {
 
     on<LoadCryptoList>((event, emit) async {
       try {
+        // только при первом заходе на экран будет показываться индикатор загрузки
+        if (state is! CryptoListLoaded){
+          emit(CryptoListLoading()); 
+        }
+
         final cryptoList = await coinsRepository.getCoinsList();
         emit(CryptoListLoaded(coinsList: cryptoList));
       } catch (e) {
         emit(CryptoListLoadingFailure(exception: e));
+      }
+      finally{
+        event.completer?.complete();
       }
     });
   }
